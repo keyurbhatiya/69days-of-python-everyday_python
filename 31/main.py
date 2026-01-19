4b1e7665ab5052047e76614c1f3c4ad130bf86d7
Keyur Bhatiya <keyurbhatiya3004@gmail.com>
2026-01-19 20:42:27 +0530

    day 31

---
+++ 31/main.py
@@ -0,0 +1,38 @@
+# Real time weather tracker
+
+import requests
+
+def get_weather(city):
+    api_key = "d21da598559ba958538f0f92cb8abd12"
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    metric = "metric"
+
+    # parameters
+
+    parameters = {
+        'q' : city,
+        'appid' : api_key,
+        "units" : metric
+    }
+
+    try : 
+        response = requests.get(base_url, params=parameters)
+        data = response.json()
+
+        if data["cod"] == 200:
+            temp = data["main"]["temp"]
+            desc = data["weather"][0]["description"]
+            humidity = data["main"]["humidity"]
+
+            print(f" \n ---- Weather in {city.capitalize()}---")
+            print(f"Temperature : {temp}c")
+            print(f"Condition : {desc.capitalize()}")
+            print(f"Humidity : {humidity}%")
+
+        else:
+            print(f"Error : {data['message']}")
+    except Exception as e:
+        print(f"An error occurred : {e}")
+
+city_input = input("Enter city name : ")
+get_weather(city_input)
