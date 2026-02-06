5d2d4be884138ae5478b9f820e89671c9768b296
Keyur Bhatiya <keyurbhatiya3004@gmail.com>
2026-02-06 12:28:34 +0530

    day 49

---
+++ 49/main.py
@@ -0,0 +1,14 @@
+# Share Your Python Code as a Single Clickable .exe File
+
+# create main.py
+
+import time
+
+def main():
+    print("Welcome to Python EXE Application ")
+    name = input("Enter your name : ")
+    print(f"Hello {name}! Your EXE is working perfectly ")
+    time.sleep(5)
+
+if __name__ == "__main__":
+    main()
